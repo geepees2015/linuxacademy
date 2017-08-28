88532616ee297166d214f5fa47e2bf0786051137
Geo Phil <gkonapoz@yahoo.com>
2017-08-28 19:23:43 +0000

    This is the corrected repo

--- cookbooks/apache/recipes/default.rb
+++ cookbooks/apache/recipes/default.rb
@@ -6,16 +6,17 @@
 #
 if node['platform_family'] == "rhel"
 	package = "httpd"
-elsif node['platform_family] == "debian"
+elsif node['platform_family'] == "debian"
 	package = "apache2"
+end
 
 package 'apache2' do
 	package_name package
 	action :install
 end
 
-service 'apache2' do
-	service_name 'httpd'
+service 'httpd' do
+	service_name package
 	action [:start, :enable]
 end
 
